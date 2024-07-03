2e41738f57aa0c9b28e88b40409d6079ed1208d4
Sangkon, Han <sigmadream@gmail.com>
2024-07-04 02:17:58 +0900

    // wip

--- ts-ui/src/components/accordion/index.jsx
+++ ts-ui/src/components/accordion/index.jsx
@@ -1,4 +1,7 @@
 import Accordion1 from "./1_r"
+import Accordion2 from "./2_r";
+import Accordion3 from "./3_r";
+import Accordion4V from "./4_v";
 import cx from "./cx"
 
 const Accordions = () => {
@@ -6,6 +9,9 @@ const Accordions = () => {
         <div className={cx("Accordions")}>
             <h2>아코디언</h2>
             <Accordion1/>
+            <Accordion2/>
+            <Accordion3/>
+            <Accordion4V/>
         </div>
     )
 }
